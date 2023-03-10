d64eaa541e3dcbc1c01371452f6c5f8ae02de27f
Joaohnt27 <126480950+Joaohnt27@users.noreply.github.com>
2023-03-09 21:43:42 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -5,17 +5,20 @@
 
 int main(int argc, char *argv[]) {
 	
-	int num1, num2, num3;
+	float num1, num2, num3, res;
 	
 	printf("Informe o primeiro numero: ");
-	scanf("%i", &num1);
+	scanf("%f", &num1);
 	
 	printf("Informe o segundo numero: ");
-	scanf("%i", &num2);
+	scanf("%f", &num2);
 	
-	num3 = num1 - num2;
+	printf("Informe o terceiro numero: ");
+	scanf("%f", &num3);
 	
-	printf("O resultado da subtracao de %i por %i e igual a: %i\n", num1, num2, num3);
+	res = num1 * num2 * num3;
+	
+	printf("O resultado da multiplicacao de %.2f x %.2f x %.2f = %.2f\n", num1, num2, num3, res);
 	
 	return 0;
 }
